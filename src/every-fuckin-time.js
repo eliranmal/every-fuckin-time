ee57a1e0f001f595f152638ff98c0d267db7bd70
eliranmal <eliranmal@gmail.com>
2016-03-12 17:44:06 +0200

    throw uuid() in, what the hack

--- src/every-fuckin-time.js
+++ src/every-fuckin-time.js
@@ -54,6 +54,14 @@
             padNumber(date.getMilliseconds(), 3);
     };
 
+    var uuid = function () {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0,
+                v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    };
+
 
     // PUBLIC API
 
@@ -63,6 +71,7 @@
     EveryFuckinTime.err = err;
     EveryFuckinTime.bla = bla;
     EveryFuckinTime.getDateTimeString = getDateTimeString;
+    EveryFuckinTime.uuid = uuid;
 
     return EveryFuckinTime;
 }));
